16e18e8845256b2de3a77685c49df6df096b5d51
MR.R4KIB <106450357+MRR4AKIB-405@users.noreply.github.com>
2022-05-30 00:33:10 +0300

    Add files via upload

---
+++ old.py
@@ -0,0 +1,52 @@
+print("""███    ███ ██████     ██████   █████  ██   ██ ██ ██████  
+████  ████ ██   ██    ██   ██ ██   ██ ██  ██  ██ ██   ██ 
+██ ████ ██ ██████     ██████  ███████ █████   ██ ██████  
+██  ██  ██ ██   ██    ██   ██ ██   ██ ██  ██  ██ ██   ██ 
+██      ██ ██   ██ ██ ██   ██ ██   ██ ██   ██ ██ ██████  
+  """)                                                     
+ 
+ 
+print(" ========= AUTHER 🔥 👿MR.RAKIB👿 =========")
+
+
+
+
+
+print("Facbook :MD.RAKIB HOOSEN")
+
+
+
+print ("FB GROUP : 🔰M.R RAKIB TERMUX COMMAND WORLD 🌍 🇧🇩( DEEPAK X JONTY)")
+print("GITHUB   : M.R RAKIB")
+
+print("NOTE : JOIN FACEBOOK GROUP FOR MORE")
+
+ 
+  
+  
+  
+ 
+  
+
+print("[1]CRACK RANDOM FB ID 2008-2011 [Just-Now-Open]")
+print("[2] EXIT")
+
+print(">>>  CHOOSE :")
+
+
+
+print("[+] TOTAL IDS CRACKING -> 20000")
+
+
+
+
+print("[!] USE , (Noakhil) FOR SEPARATOR")
+print("EXAMPLE : 123456,1234567,123456789")
+print("NOTE : USEFULL PASSWORD IS 123456")
+print("[?] ENTER PASSWORD :")
+
+
+print("[+] OK RESULTS SAVED IN -> ok.txt")
+
+print("[+] CP RESULTS SAVED IN -> cp.txt")
+print("[!] IF NO RESULT TURN ON AIRPLANE MODE 5 SECONDS")
